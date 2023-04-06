6b59f0f5a0a914173ec5c6f98336f81e09e429cb
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-04-06 14:42:28 -0500

    first commit

---
+++ src/main/java/tethergroup/tether/TetherApplication.java
@@ -0,0 +1,13 @@
+package tethergroup.tether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetherApplication.class, args);
+	}
+
+}
