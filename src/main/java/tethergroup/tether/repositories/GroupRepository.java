1d2fc324990436ba94c82119f93225ac6e6ecb8e
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-04-10 12:24:54 -0500

    working on showing my groups

--- src/main/java/tethergroup/tether/repositories/GroupRepository.java
+++ src/main/java/tethergroup/tether/repositories/GroupRepository.java
@@ -8,11 +8,12 @@ import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
 //    random group query for visitor
-    @Query(nativeQuery = true, value= "SELECT * FROM groups ORDER BY random() LIMIT 50")
+    @Query(nativeQuery = true, value= "SELECT * FROM groups ORDER BY rand() LIMIT 50")
     List<Group> randomGroups();
 
 //    query for users groups on the latest group created that they are in
-    @Query(nativeQuery = true, value = "SELECT g.name, g.description FROM groups g ORDER BY g.id DESC")
+    @Query(nativeQuery = true, value = "SELECT * FROM groups g ORDER BY g.id DESC")
     List<Group> groupsByDescendingId();
 
+    Group searchByGroupName(String name);
 }
