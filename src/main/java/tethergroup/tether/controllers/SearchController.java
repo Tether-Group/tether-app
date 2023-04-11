982ffe4d3fb59fd0fe4053694973d7b7efd4d9ea
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-04-11 15:01:11 -0500

    working on search bar results

--- src/main/java/tethergroup/tether/controllers/SearchController.java
+++ src/main/java/tethergroup/tether/controllers/SearchController.java
@@ -1,15 +1,15 @@
 package tethergroup.tether.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
+import org.springframework.lang.Nullable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import tethergroup.tether.models.Group;
-import tethergroup.tether.models.Post;
-import tethergroup.tether.models.User;
 import tethergroup.tether.repositories.GroupRepository;
 import tethergroup.tether.repositories.PostRepository;
 import tethergroup.tether.repositories.UserRepository;
@@ -20,19 +20,19 @@ import java.util.List;
 @Controller
 public class SearchController {
 
-    private PostRepository postDao;
-    private UserRepository userDao;
-    private GroupRepository groupDao;
+    private final PostRepository postDao;
+    private final UserRepository userDao;
+    private final GroupRepository groupDao;
 
-    @GetMapping("/search-results")
-    public String globalGETSearch(Model model, @RequestParam(name = "global-search") String group) {
-        return "posts/search-results";
-    }
-
-    @PostMapping("/search-results")
-    public String globalPOSTSearch(Model model, @RequestParam(name = "global-search") String group) {
-//        List<Group> searchedGroups = groupDao.searchByGroupNameLike(group);
-//        model.addAttribute("searchedGroups", searchedGroups);
+    @GetMapping("/posts/search-results")
+    public String globalSearch(@RequestParam(name = "search") @Nullable String search, Model model) {
+        List<Group> searchedGroups;
+        if (search != null) {
+            searchedGroups = groupDao.findLikeGroupNameOrDescription(search);
+        } else {
+            searchedGroups = groupDao.findAll();
+        }
+        model.addAttribute("searchedGroups", searchedGroups);
 
         return "posts/search-results";
     }
