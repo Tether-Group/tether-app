0dd01102a3a79f44564177b4744fe51277ce1cb9
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-04-11 11:14:55 -0500

    Added create a post functionality

--- src/main/java/tethergroup/tether/controllers/PostController.java
+++ src/main/java/tethergroup/tether/controllers/PostController.java
@@ -1,22 +1,28 @@
 package tethergroup.tether.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import tethergroup.tether.models.Group;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import tethergroup.tether.models.Post;
+import tethergroup.tether.repositories.GroupRepository;
 import tethergroup.tether.repositories.PostRepository;
+import tethergroup.tether.repositories.PostTypeRepository;
 import tethergroup.tether.repositories.UserRepository;
 
-import java.util.List;
+import java.sql.Timestamp;
+import java.util.Date;
 
 @RequiredArgsConstructor
 @Controller
 public class PostController {
 
     private final PostRepository postDao;
+    private final UserRepository userDao;
+    private final GroupRepository grouDao;
+    private final PostTypeRepository postTypeDao;
 
     @GetMapping("/posts")
     public String posts() {
@@ -24,7 +30,28 @@ public class PostController {
     }
 
     @GetMapping("/post/create")
-    public String createPost(){
+    public String createPost(Model model){
+        model.addAttribute("post", new Post());
         return "posts/create-post";
     }
+
+
+    @PostMapping("/post/create/text")
+    public String createPost(
+                             @RequestParam("header") String title,
+                             @RequestParam("body") String body) {
+        Post post = new Post();
+        post.setUser(userDao.findById(7L).get());
+        post.setGroup(grouDao.findById(3L).get());
+        post.setHeader(title);
+        post.setBody(body);
+        post.setPostType(postTypeDao.findById(1L).get());
+        Timestamp timestamp = new Timestamp((new Date()).getTime());
+        post.setPostDate(timestamp);
+        postDao.save(post);
+        return "index";
+    }
+
+
+
 }
--- src/main/java/tethergroup/tether/controllers/UserController.java
+++ src/main/java/tethergroup/tether/controllers/UserController.java
@@ -1,16 +1,12 @@
 package tethergroup.tether.controllers;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import tethergroup.tether.models.Group;
 import tethergroup.tether.models.User;
 import tethergroup.tether.repositories.UserRepository;
-
-import java.util.List;
 
 @Controller
 public class UserController {
