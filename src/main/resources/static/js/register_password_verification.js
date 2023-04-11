c3934e31b6efedd0221534d8b4490f65ef2038dc
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-04-11 09:51:25 -0500

    client side password verification working

--- src/main/resources/static/js/register_password_verification.js
+++ src/main/resources/static/js/register_password_verification.js
@@ -1,31 +1,51 @@
 "use strict";
 
-let myInput = $('#register-password');
-let lowercase = $('#lowercase');
-let uppercase = $('#uppercase');
-let number = $('#number');
-let length = $('#length');
-let special = $('#special');
-let noSpace = $('#empty');
-let mySecondInput = $('#register-pw-verification');
+let myInput = document.getElementById('register-password');
+let lowercase = document.getElementById('lowercase');
+let uppercase = document.getElementById('uppercase');
+let number = document.getElementById('number');
+let length = document.getElementById('length');
+let special = document.getElementById('special');
+let noSpace = document.getElementById('empty');
+let pwMatch = document.getElementById('password-match');
+let mySecondInput = document.getElementById('register-pw-verification');
+
+let lowerCaseLetters = /[a-z]/;
+let upperCaseLetters = /[A-Z]/g;
+let numbers = /[0-9]/g;
+let specialChars = /[ !"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/g;
+let emptySpaces = /\s/g;
 
 // let passwordInvalid = true;
 // let disableBtn = !myInput.value.length && passwordInvalid;
 
+document.getElementById('submit').disabled = true;
+
 // When the user clicks on the password field, show the message box
 myInput.onfocus = function () {
-    $('#message').css('display', 'initial');
+    document.getElementById('message').style.display = 'block';
+    // $('#message').css('display', 'initial');
+}
+
+mySecondInput.onfocus = function () {
+    document.getElementById('message').style.display = 'block';
+    // $('#message').css('display', 'initial');
 }
 
 // When the user clicks outside the password field, hide the message box
 myInput.onblur = function () {
-    $('#message').css('display', 'none');
+    document.getElementById('message').style.display = 'none';
+    // $('#message').css('display', 'none');
+}
+
+mySecondInput.onblur = function () {
+    document.getElementById('message').style.display = 'none';
+    // $('#message').css('display', 'none');
 }
 
 // When the user starts to type something inside the password field
 myInput.onkeyup = function () {
     // Validate lowercase letters
-    let lowerCaseLetters = /[a-z]/g;
     if (myInput.value.match(lowerCaseLetters)) {
         lowercase.classList.remove("invalid");
         lowercase.classList.add("valid");
@@ -35,7 +55,6 @@ myInput.onkeyup = function () {
     }
 
     // Validate capital letters
-    let upperCaseLetters = /[A-Z]/g;
     if (myInput.value.match(upperCaseLetters)) {
         uppercase.classList.remove("invalid");
         uppercase.classList.add("valid");
@@ -45,7 +64,6 @@ myInput.onkeyup = function () {
     }
 
     // Validate numbers
-    let numbers = /[0-9]/g;
     if (myInput.value.match(numbers)) {
         number.classList.remove("invalid");
         number.classList.add("valid");
@@ -64,7 +82,6 @@ myInput.onkeyup = function () {
     }
 
     // Validate special characters
-    let specialChars = /[ !"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/g;
     if (myInput.value.match(specialChars)) {
         special.classList.remove("invalid");
         special.classList.add("valid");
@@ -74,7 +91,6 @@ myInput.onkeyup = function () {
     }
 
     // Validate empty spaces
-    let emptySpaces = /\s/g;
     if (!myInput.value.match(emptySpaces)) {
         noSpace.classList.remove('invalid');
         noSpace.classList.add('valid');
@@ -84,26 +100,34 @@ myInput.onkeyup = function () {
     }
 
     // Validate verification pw
-    if (myInput.value.match(mySecondInput)) {
-        mySecondInput.classList.remove('invalid');
-        mySecondInput.classList.add('valid');
-    } else {
-        mySecondInput.classList.remove('valid');
-        mySecondInput.classList.add('invalid');
+    finalValidate();
+
+    mySecondInput.onkeyup = function () {
+        finalValidate();
     }
 
-    // if (myInput.value.match(lowerCaseLetters)
-    //     && myInput.value.match(upperCaseLetters)
-    //     && myInput.value.match(numbers)
-    //     && myInput.value.length >= 8
-    //     && myInput.value.length <= 20
-    //     && myInput.value.match(specialChars)
-    //     && !myInput.value.match(emptySpaces)
-    //     && myInput.value.match(mySecondInput)) {
-    //     passwordInvalid = false;
-    // } else {
-    //     passwordInvalid = true;
-    // }
-    //
-    // disableBtn = !myInput.value.length && passwordInvalid;
+
+    function finalValidate() {
+        // Validate verification pw
+        if (myInput.value === mySecondInput.value) {
+            pwMatch.classList.remove('invalid');
+            pwMatch.classList.add('valid');
+        } else {
+            pwMatch.classList.remove('valid');
+            pwMatch.classList.add('invalid');
+        }
+
+        if (myInput.value.match(lowerCaseLetters)
+            && myInput.value.match(upperCaseLetters)
+            && myInput.value.match(numbers)
+            && myInput.value.length >= 8
+            && myInput.value.length <= 20
+            && myInput.value.match(specialChars)
+            && !myInput.value.match(emptySpaces)
+            && myInput.value === mySecondInput.value) {
+            document.getElementById('submit').disabled = false;
+        } else {
+            document.getElementById('submit').disabled = true;
+        }
+    }
 }
