dc5b884c3f65be60e59401973925f52bf6e1cf7d
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-04-12 12:49:37 -0500

    trying to re-factor create to make the update easier

--- src/main/java/tethergroup/tether/controllers/GroupController.java
+++ src/main/java/tethergroup/tether/controllers/GroupController.java
@@ -2,12 +2,10 @@ package tethergroup.tether.controllers;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import tethergroup.tether.models.Group;
 import tethergroup.tether.models.PostType;
 import tethergroup.tether.models.User;
@@ -50,30 +48,46 @@ public class GroupController {
     }
 
     @GetMapping("/group/create")
-    public String returnGroupCreatePage() {
+    public String returnGroupCreatePage(Model model) {
+        model.addAttribute("group", new Group());
         return "groups/create-group";
     }
 
     @PostMapping("/group/create")
-    public String createGroup(
-            @RequestParam("group-name") String name,
-                              @RequestParam("description") String description,
-                              @RequestParam(value = "visibility", required = false) boolean isPrivate,
-                              @RequestParam(value = "post-types", required = false) String[] postTypesStrings,
-                              Principal principal) {
-        List<PostType> postTypes = new ArrayList<>();
-        for (String string : postTypesStrings) {
-            postTypes.add(postTypeDao.findById((long) Integer.parseInt(string)).get());
-        }
-        Group group = new Group();
-        group.setName(name);
-        group.setDescription(description);
-        group.setPrivate(isPrivate);
-        group.setPostTypes(postTypes);
-        group.setAdmin(userDao.findById(7L).get());
+    public String createGroup(@ModelAttribute("group") Group group) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        group.setAdmin(loggedInUser);
+        group.setPostTypes(group.getPostTypes());
+        group.setName(group.getName());
+        group.setDescription(group.getDescription());
+        group.setPrivate(group.isPrivate());
         groupDao.save(group);
+
         return "redirect:/groups";
     }
+
+//    @PostMapping("/group/create")
+//    public String createGroup(
+//            @RequestParam("group-name") String name,
+//            @RequestParam("description") String description,
+//            @RequestParam(value = "visibility", required = false) boolean isPrivate,
+//            @RequestParam(value = "post-types", required = false) String[] postTypesStrings,
+//            Principal principal)
+//    {
+//        List<PostType> postTypes = new ArrayList<>();
+//        for (String string : postTypesStrings) {
+//            postTypes.add(postTypeDao.findById((long) Integer.parseInt(string)).get());
+//        }
+//        Group group = new Group();
+//        group.setName(name);
+//        group.setDescription(description);
+//        group.setPrivate(isPrivate);
+//        group.setPostTypes(postTypes);
+//        group.setAdmin(userDao.findById(7L).get());
+//        groupDao.save(group);
+//        return "redirect:/groups";
+//    }
 
     @GetMapping("/group/{groupId}")
     public String addGroupAttributeToGroupPage(Model model, @PathVariable Long groupId) {
