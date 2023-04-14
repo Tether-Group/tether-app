c2133c680e6c408187c9cdd0e3390d2aca91a5e5
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-04-13 23:05:53 -0500

    working on notification counts

---
+++ src/main/java/tethergroup/tether/controllers/NotificationController.java
@@ -0,0 +1,44 @@
+package tethergroup.tether.controllers;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import tethergroup.tether.models.Membership;
+import tethergroup.tether.models.User;
+import tethergroup.tether.repositories.GroupRepository;
+import tethergroup.tether.repositories.MembershipRepository;
+import tethergroup.tether.repositories.PostTypeRepository;
+import tethergroup.tether.repositories.UserRepository;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Controller
+public class NotificationController {
+
+    private final GroupRepository groupDao;
+    private final UserRepository userDao;
+    private final PostTypeRepository postTypeDao;
+    private final MembershipRepository membershipDao;
+
+    //    TODO: * For admins, check the memberships table, see if they have any pending requests, get the count of those
+    //     requests for those groups that they are the admin of, and display them on the page if they are logged in, if
+    //     they are the admin.Then, for each request, display forms for accept/deny which will make the user join/reject
+    //     the membership based on the admin’s decision.
+
+    @GetMapping("/notifications")
+    public String showNotifications(Model model) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("loggedInUser", loggedInUser);
+
+        List<Long> groupRequests = membershipDao.findAdminsPendingGroupRequestsCount(loggedInUser.getId());
+        System.out.println(groupRequests);
+
+        return "users/notifications";
+//        return "redirect:/error";
+    }
+}
--- src/main/java/tethergroup/tether/repositories/MembershipRepository.java
+++ src/main/java/tethergroup/tether/repositories/MembershipRepository.java
@@ -1,6 +1,8 @@
 package tethergroup.tether.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tethergroup.tether.models.Membership;
 import tethergroup.tether.models.User;
 
@@ -9,4 +11,10 @@ import java.util.List;
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
 
     Membership findMembershipByUser_IdAndGroup_Id(Long userId, Long groupId);
+
+    List<Membership> findMembershipsByUser_Id(Long userId);
+
+    @Query(nativeQuery = true,
+            value = "SELECT group_id FROM memberships m INNER JOIN groups g ON g.id = m.group_id INNER JOIN users u ON g.admin_id = :id WHERE m.is_pending = 1")
+    List<Long> findAdminsPendingGroupRequestsCount(@Param("id")Long id);
 }
