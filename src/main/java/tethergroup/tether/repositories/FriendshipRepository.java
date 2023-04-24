31e356fce8d4a0174c05f36992147b9e58ebfa75
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-04-23 23:33:07 -0500

    Groups can add cover photos - integrated filestack into create group page and edit group form

--- src/main/java/tethergroup/tether/repositories/FriendshipRepository.java
+++ src/main/java/tethergroup/tether/repositories/FriendshipRepository.java
@@ -11,7 +11,9 @@ public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 
     Friendship findByRequester_IdAndAcceptor_Id(Long requesterId, Long acceptorId);
 
-    List<Friendship> friendsOfUser(Long acceptorId);
+    @Query(nativeQuery = true,
+    value = "select * from friendships f where acceptor = :user_id or requester = :user_id and is_pending = 0")
+    List<Friendship> getFriendshipsOfUser(@Param("user_id") Long userId);
 
     @Query(nativeQuery = true,
             value = "SELECT count(id) FROM friendships f WHERE f.acceptor = :user_id AND is_pending = 1")
