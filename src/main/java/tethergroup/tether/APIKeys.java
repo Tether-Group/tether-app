855e9cdafbc3b05a17b44986fa587192322a6fe3
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-11-10 17:32:18 -0600

    updating heroku props file and edited APIKeys class

--- src/main/java/tethergroup/tether/APIKeys.java
+++ src/main/java/tethergroup/tether/APIKeys.java
@@ -5,11 +5,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class APIKeys {
-    @Value("${talkJSAppKey}")
+
+    @Value("${TALK_JS_APP_KEY}")
     public String talkJSAppKey;
 
     @Value("${MAPBOX_API_KEY}")
-    public String MapboxAPIKey;
+    public String mapBoxAPIKey;
 
     @Value("${FILESTACK_API_KEY}")
     public String filestackAPIKey;
